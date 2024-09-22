83d3710b5e5602724259cf92b665c86049cf9e4c
Valentin <valentin.breuss@baur.eu>
2024-09-22 09:40:35 +0200

    Add Example 1.2

---
+++ NatureOfCode.Base/Vector.cs
@@ -0,0 +1,77 @@
+﻿
+namespace NatureOfCode.Base
+{
+    public class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public double Magnitude => CalculateMagnitude(X, Y, Z);
+
+        public Vector(double x, double y, double z = 0.0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public void Normalize()
+        {
+            DivideBy(Magnitude);
+        }
+
+        public void Add(Vector vector)
+        {
+            X += vector.X;
+            Y += vector.Y;
+            Z += vector.Z;
+        }
+
+        public void Subtract(Vector vector)
+        {
+            X -= vector.X;
+            Y -= vector.Y;
+            Z -= vector.Z;
+        }
+
+        public void MultiplyWith(double scalar)
+        {
+            X *= scalar;
+            Y *= scalar;
+            Z *= scalar;
+        }
+
+        public void DivideBy(double scalar)
+        {
+            X /= scalar;
+            Y /= scalar;
+            Z /= scalar;
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+        public static Vector operator *(Vector v, double scalar)
+        {
+            return new Vector(v.X * scalar, v.Y * scalar, v.Z * scalar);
+        }
+
+        public static Vector operator /(Vector v, double scalar)
+        {
+            return new Vector(v.X / scalar, v.Y / scalar, v.Z / scalar);
+        }
+
+        private static double CalculateMagnitude(double x, double y, double z)
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+    }
+}
---
+++ NatureOfCode.Examples/1/Example1_2.cs
@@ -0,0 +1,25 @@
+﻿namespace NatureOfCode;
+
+[Sketch(Description = "Example 1.2: Bouncing Ball with Vectors!")]
+public class Example1_2 : Sketch
+{
+    Vector Position = new Vector(100.0, 100.0);
+    Vector Velocity = new Vector(2.5, 2.0);
+
+    public override void Draw()
+    {
+        Canvas.Reset();
+
+        Position = Position + Velocity;
+        if (Position.X > Canvas.Width || Position.X < 0)
+        {
+            Velocity.X = Velocity.X * -1.0;
+        }
+        if (Position.Y > Canvas.Height || Position.Y < 0)
+        {
+            Velocity.Y = Velocity.Y * -1.0;
+        }
+
+        Canvas.DrawCircle(Position.X, Position.Y, 48);
+    }
+}
